3626b34a892116dec858b6042668428cd5da95ae
diwas <lunaticoda123@gmail.com>
2024-04-23 18:22:04 +0545

    File buffer activation

--- cyanide-compiler.cpp
+++ cyanide-compiler.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
-int main(){
-    std::cout << "Hello" ;
+int main(int argc, char *argv[]){
+    if(argc < 2){
+        std::cout << "cyanide example.cy" << std::endl ;
+        exit(1) ;
+    }
+
+    std::cout << "Input: " << argv[1] << std::endl ;
+    std::ifstream fileStr(argv[1]) ;
+    std::stringstream buffer ;
+    char temp ;
+
+    while(fileStr.get(temp)){
+        buffer << temp ;
+    }
+    
+    std::cout << "Source Code: \n" <<  buffer.str() << std::endl ;
+
     return 0 ;
 }
